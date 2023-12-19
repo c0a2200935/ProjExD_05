b513f585ce95cdd29e65d44fd2d6519224306907
sugimoto <c0b22079b2@edu.teu.ac.jp>
2023-12-19 17:27:05 +0900

    DROP1

--- shootinggame.py
+++ shootinggame.py
@@ -1,190 +1,329 @@
-import pygame
-import sys
-import random
 import math
+import os
+import random
+import sys
+import time
+import pygame as pg
 
-class Player:
-    def __init__(self, x, y, size, speed, bullet_speed):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.speed = speed
-        self.bullet_speed = bullet_speed
-        self.bullets = []
 
-    def move(self, direction):
-        if direction == "left":
-            self.x -= self.speed
-        elif direction == "right":
-            self.x += self.speed
-        # 画面外にプレイヤーが出ないように制限
-        self.x = max(0, min(width - self.size, self.x))
+WIDTH = 1600  # ゲームウィンドウの幅
+HEIGHT = 900  # ゲームウィンドウの高さ
+MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
-    def shoot(self):
-        self.bullets.append(Bullet(self.x + self.size // 2, self.y, self.bullet_speed))
+
+def check_bound(obj: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内か画面外かを判定し，真理値タプルを返す
+    引数 obj：オブジェクト（爆弾，こうかとん，ビーム）SurfaceのRect
+    戻り値：横方向，縦方向のはみ出し判定結果（画面内：True／画面外：False）
+    """
+    yoko, tate = True, True
+    if obj.left < 0 or WIDTH < obj.right:  # 横方向のはみ出し判定
+        yoko = False
+    if obj.top < 0 or HEIGHT < obj.bottom:  # 縦方向のはみ出し判定
+        tate = False
+    return yoko, tate
+
+
+def calc_orientation(org: pg.Rect, dst: pg.Rect) -> tuple[float, float]:
+    """
+    orgから見て，dstがどこにあるかを計算し，方向ベクトルをタプルで返す
+    引数1 org：爆弾SurfaceのRect
+    引数2 dst：こうかとんSurfaceのRect
+    戻り値：orgから見たdstの方向ベクトルを表すタプル
+    """
+    x_diff, y_diff = dst.centerx-org.centerx, dst.centery-org.centery
+    norm = math.sqrt(x_diff**2+y_diff**2)
+    return x_diff/norm, y_diff/norm
+
+
+class Bird(pg.sprite.Sprite):
+    """
+    ゲームキャラクター（こうかとん）に関するクラス
+    """
+    delta = {  # 押下キーと移動量の辞書
+        pg.K_UP: (0, -1),
+        pg.K_DOWN: (0, +1),
+        pg.K_LEFT: (-1, 0),
+        pg.K_RIGHT: (+1, 0),
+    }
+
+    def __init__(self, num: int, xy: tuple[int, int]):
+        """
+        こうかとん画像Surfaceを生成する
+        引数1 num：こうかとん画像ファイル名の番号
+        引数2 xy：こうかとん画像の位置座標タプル
+        """
+        super().__init__()
+        img0 = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 0, 2.0)
+        img = pg.transform.flip(img0, True, False)  # デフォルトのこうかとん
+        self.imgs = {
+            (+1, 0): img,  # 右
+            (+1, -1): pg.transform.rotozoom(img, 45, 1.0),  # 右上
+            (0, -1): pg.transform.rotozoom(img, 90, 1.0),  # 上
+            (-1, -1): pg.transform.rotozoom(img0, -45, 1.0),  # 左上
+            (-1, 0): img0,  # 左
+            (-1, +1): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
+            (0, +1): pg.transform.rotozoom(img, -90, 1.0),  # 下
+            (+1, +1): pg.transform.rotozoom(img, -45, 1.0),  # 右下
+        }
+        self.dire = (+1, 0)
+        self.image = self.imgs[self.dire]
+        self.rect = self.image.get_rect()
+        self.rect.center = xy
+        self.speed = 10
+
+    def change_img(self, num: int, screen: pg.Surface):
+        """
+        こうかとん画像を切り替え，画面に転送する
+        引数1 num：こうかとん画像ファイル名の番号
+        引数2 screen：画面Surface
+        """
+        self.image = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/{num}.png"), 0, 2.0)
+        screen.blit(self.image, self.rect)
+
+    def update(self, key_lst: list[bool], screen: pg.Surface):
+        """
+        押下キーに応じてこうかとんを移動させる
+        引数1 key_lst：押下キーの真理値リスト
+        引数2 screen：画面Surface
+        """
+        sum_mv = [0, 0]
+        for k, mv in __class__.delta.items():
+            if key_lst[k]:
+                self.rect.move_ip(+self.speed*mv[0], +self.speed*mv[1])
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        if check_bound(self.rect) != (True, True):
+            for k, mv in __class__.delta.items():
+                if key_lst[k]:
+                    self.rect.move_ip(-self.speed*mv[0], -self.speed*mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.dire = tuple(sum_mv)
+            self.image = self.imgs[self.dire]
+        screen.blit(self.image, self.rect)
+
+
+class Bomb(pg.sprite.Sprite):
+    """
+    爆弾に関するクラス
+    """
+    colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255)]
+
+    def __init__(self, emy: "Enemy", bird: Bird):
+        """
+        爆弾円Surfaceを生成する
+        引数1 emy：爆弾を投下する敵機
+        引数2 bird：攻撃対象のこうかとん
+        """
+        super().__init__()
+        rad = random.randint(10, 50)  # 爆弾円の半径：10以上50以下の乱数
+        color = random.choice(__class__.colors)  # 爆弾円の色：クラス変数からランダム選択
+        self.image = pg.Surface((2*rad, 2*rad))
+        pg.draw.circle(self.image, color, (rad, rad), rad)
+        self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        # 爆弾を投下するemyから見た攻撃対象のbirdの方向を計算
+        self.vx, self.vy = calc_orientation(emy.rect, bird.rect)  
+        self.rect.centerx = emy.rect.centerx
+        self.rect.centery = emy.rect.centery+emy.rect.height/2
+        self.speed = 6
+
+    def update(self):
+        """
+        爆弾を速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        self.rect.move_ip(+self.speed*self.vx, +self.speed*self.vy)
+        if check_bound(self.rect) != (True, True):
+            self.kill()
+
+
+class Beam(pg.sprite.Sprite):
+    """
+    ビームに関するクラス
+    """
+    def __init__(self, bird: Bird):
+        """
+        ビーム画像Surfaceを生成する
+        引数 bird：ビームを放つこうかとん
+        """
+        super().__init__()
+        self.vx, self.vy = bird.dire
+        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        self.image = pg.transform.rotozoom(pg.image.load(f"{MAIN_DIR}/fig/beam.png"), angle, 2.0)
+        self.vx = math.cos(math.radians(angle))
+        self.vy = -math.sin(math.radians(angle))
+        self.rect = self.image.get_rect()
+        self.rect.centery = bird.rect.centery+bird.rect.height*self.vy
+        self.rect.centerx = bird.rect.centerx+bird.rect.width*self.vx
+        self.speed = 10
+
+    def update(self):
+        """
+        ビームを速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        self.rect.move_ip(+self.speed*self.vx, +self.speed*self.vy)
+        if check_bound(self.rect) != (True, True):
+            self.kill()
+
+
+class Explosion(pg.sprite.Sprite):
+    """
+    爆発に関するクラス
+    """
+    def __init__(self, obj: "Bomb|Enemy", life: int):
+        """
+        爆弾が爆発するエフェクトを生成する
+        引数1 obj：爆発するBombまたは敵機インスタンス
+        引数2 life：爆発時間
+        """
+        super().__init__()
+        img = pg.image.load(f"{MAIN_DIR}/fig/explosion.gif")
+        self.imgs = [img, pg.transform.flip(img, 1, 1)]
+        self.image = self.imgs[0]
+        self.rect = self.image.get_rect(center=obj.rect.center)
+        self.life = life
+
+    def update(self):
+        """
+        爆発時間を1減算した爆発経過時間_lifeに応じて爆発画像を切り替えることで
+        爆発エフェクトを表現する
+        """
+        self.life -= 1
+        self.image = self.imgs[self.life//10%2]
+        if self.life < 0:
+            self.kill()
+
+
+class Enemy(pg.sprite.Sprite):
+    """
+    敵機に関するクラス
+    """
+    imgs = [pg.image.load(f"{MAIN_DIR}/fig/alien{i}.png") for i in range(1, 4)]
     
-    def draw(self):
-        pygame.draw.circle(screen, white, (self.x + self.size // 2, self.y + self.size // 2), self.size // 2)
+    def __init__(self):
+        super().__init__()
+        self.image = random.choice(__class__.imgs)
+        self.rect = self.image.get_rect()
+        self.rect.center = random.randint(0, WIDTH), 0
+        self.vy = +6
+        self.bound = random.randint(50, HEIGHT/2)  # 停止位置
+        self.state = "down"  # 降下状態or停止状態
+        self.interval = random.randint(50, 300)  # 爆弾投下インターバル
 
-class Enemy:
-    def __init__(self, x, y, size, speed):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.speed = speed
-        self.is_alive = True
+    def update(self):
+        """
+        敵機を速度ベクトルself.vyに基づき移動（降下）させる
+        ランダムに決めた停止位置_boundまで降下したら，_stateを停止状態に変更する
+        引数 screen：画面Surface
+        """
+        if self.rect.centery > self.bound:
+            self.vy = 0
+            self.state = "stop"
+        self.rect.centery += self.vy
 
-    def move(self):
-        self.y += self.speed
 
-    def draw(self):
-        pygame.draw.rect(screen, white, (self.x, self.y, self.size, self.size))
+class Score:
+    """
+    打ち落とした爆弾，敵機の数をスコアとして表示するクラス
+    爆弾：1点
+    敵機：10点
+    """
+    def __init__(self):
+        self.font = pg.font.Font(None, 50)
+        self.color = (0, 0, 255)
+        self.value = 0
+        self.image = self.font.render(f"Score: {self.value}", 0, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.center = 100, HEIGHT-50
 
-class Bullet:
-    def __init__(self, x, y, speed):
-        self.x = x
-        self.y = y
-        self.speed = speed
+    def update(self, screen: pg.Surface):
+        self.image = self.font.render(f"Score: {self.value}", 0, self.color)
+        screen.blit(self.image, self.rect)
 
-    def move(self):
-        self.y -= self.speed
 
-    def draw(self):
-        pygame.draw.circle(screen, white, (self.x, self.y), 5)
+class Drop(pg.sprite.Sprite):
+    def __init__(self,enemy: Enemy):
+        super().__init__()
+        self.image = pg.Surface((15, 15))
+        pg.draw.rect(self.image, (random.randint(0,255),random.randint(0,255),random.randint(0,255)),(0, 0, 15, 15))
+        self.rect= self.image.get_rect(center=enemy.rect.center)
+     
+    def update(self):
+        self.rect.centery += 3
+        
+def main():
+    pg.display.set_caption("真！こうかとん無双")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load(f"{MAIN_DIR}/fig/pg_bg.jpg")
+    score = Score()
+    bird = Bird(3, (900, 400))
+    bombs = pg.sprite.Group()
+    beams = pg.sprite.Group()
+    exps = pg.sprite.Group()
+    emys = pg.sprite.Group()
+    drops = pg.sprite.Group()
+    tmr = 0
+    clock = pg.time.Clock()
+    while True:
+        key_lst = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return 0
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                beams.add(Beam(bird))
+        screen.blit(bg_img, [0, 0])
 
-class Item:
-    def __init__(self, x, y, speed):
-        self.x = x
-        self.y = y
-        self.speed = speed
+        if tmr%200 == 0:  # 200フレームに1回，敵機を出現させる
+            emys.add(Enemy())
 
-    def move(self):
-        self.y += self.speed
+        for emy in emys:
+            if emy.state == "stop" and tmr%emy.interval == 0:
+                # 敵機が停止状態に入ったら，intervalに応じて爆弾投下
+                bombs.add(Bomb(emy, bird))
 
-    def draw(self):
-        pygame.draw.rect(screen, (0, 255, 0), (self.x, self.y, 20, 20))
+        for emy in pg.sprite.groupcollide(emys, beams, True, True).keys():
+            exps.add(Explosion(emy, 100))  # 爆発エフェクト
+            score.value += 10  # 10点アップ
+            bird.change_img(6, screen)  # こうかとん喜びエフェクト
+            if random.randint(0,100)<100:
+                drops.add(Drop(emy))
 
-# 初期化
-pygame.init()
+        for bomb in pg.sprite.groupcollide(bombs, beams, True, True).keys():
+            exps.add(Explosion(bomb, 50))  # 爆発エフェクト
+            score.value += 1  # 1点アップ
 
-# 画面設定
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("横スクロールシューティングゲーム")
+        if len(pg.sprite.spritecollide(bird, bombs, True)) != 0:
+            bird.change_img(8, screen) # こうかとん悲しみエフェクト
+            score.update(screen)
+            pg.display.update()
+            time.sleep(2)
+            return
+        
+        for drop in pg.sprite.spritecollide(bird, drops, True):
+            pass
 
-# 色の設定
-white = (255, 255, 255)
-black = (0, 0, 0)
+        bird.update(key_lst, screen)
+        beams.update()
+        beams.draw(screen)
+        emys.update()
+        emys.draw(screen)
+        bombs.update()
+        bombs.draw(screen)
+        exps.update()
+        exps.draw(screen)
+        score.update(screen)
+        drops.update()
+        drops.draw(screen)
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
 
-# プレイヤーの初期位置と速度
-player = Player(width // 2 - 25, height - 100, 50, 10, 15)
 
-# 敵の初期位置と速度
-enemy_speed = 5
-enemies = []
-
-# アイテムの初期速度
-item_speed = 5
-items = []
-
-# フォントの設定
-font = pygame.font.Font(None, 36)
-
-def spawn_enemy():
-    if random.randint(0, 100) < 5:
-        enemy_x = random.randint(0, width - 50)
-        enemy_y = 0
-        enemies.append(Enemy(enemy_x, enemy_y, 50, enemy_speed))
-
-def spawn_item(x, y):
-    items.append(Item(x, y, item_speed))
-
-# ゲームループ
-clock = pygame.time.Clock()
-score = 0
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player.move("left")
-    elif keys[pygame.K_RIGHT]:
-        player.move("right")
-
-    # 弾の発射
-    if keys[pygame.K_SPACE]:
-        player.shoot()
-
-    # 敵とアイテムの生成
-    spawn_enemy()
-
-    # アイテムの移動
-    for item in items:
-        item.move()
-
-    # アイテムの描画
-    for item in items:
-        item.draw()
-
-    # 弾とアイテムの移動および衝突判定
-    for bullet in player.bullets:
-        bullet.move()
-        for enemy in enemies:
-            if (
-                enemy.x < bullet.x < enemy.x + enemy.size
-                and enemy.y < bullet.y < enemy.y + enemy.size
-            ):
-                player.bullets.remove(bullet)
-                enemies.remove(enemy)
-                score += 1
-                if random.randint(0, 100) < 50:  # 50%の確率でアイテムドロップ
-                    spawn_item(enemy.x + enemy.size // 2, enemy.y + enemy.size // 2)
-
-    # アイテムとプレイヤーの衝突判定
-    for item in items:
-        if (
-            player.x < item.x < player.x + player.size
-            and player.y < item.y < player.y + player.size
-        ):
-            items.remove(item)
-            player.bullet_speed = 20  # アイテム取得で弾の速度を上げる
-            for angle in range(0, 360, 120):  # 120度ごとに弾を追加
-                player.bullets.append(Bullet(player.x + player.size // 2, player.y, player.bullet_speed))
-
-    # 敵の移動およびプレイヤーとの衝突判定
-    for enemy in enemies:
-        enemy.move()
-        if (
-            player.x < enemy.x < player.x + player.size
-            and player.y < enemy.y < player.y + player.size
-        ):
-            pygame.quit()
-            sys.exit()
-
-    # 画面のクリア
-    screen.fill(black)
-
-    # プレイヤーの描画
-    player.draw()
-
-    # 敵の描画
-    for enemy in enemies:
-        enemy.draw()
-
-    # 弾の描画
-    for bullet in player.bullets:
-        bullet.draw()
-
-    # スコアの表示
-    score_text = font.render("Score: " + str(score), True, white)
-    screen.blit(score_text, (10, 10))
-
-    # 画面の更新
-    pygame.display.flip()
-
-    # フレームレートの設定
-    clock.tick(30)
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
